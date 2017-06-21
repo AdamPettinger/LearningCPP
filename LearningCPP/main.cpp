34f1b6e3b882e6c8e9c4d1f2c7187f4d9329362b
AdamPettinger <adam.pettinger@utexas.edu>
2017-06-21 15:49:38 -0600

    Initial Commit

---
+++ LearningCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearningCPP
+//
+//  Created by Adam on 6/21/17.
+//  Copyright © 2017 Adam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
